72fa38d159ffde0fa57b3b1725c8c08b56b7420b
HaloTwo <lsm6265@naver.com>
2025-04-18 18:30:07 +0900

    11

--- Source/Warrior/Warrior.Build.cs
+++ Source/Warrior/Warrior.Build.cs
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using UnrealBuildTool.Rules;
 
 public class Warrior : ModuleRules
 {
@@ -8,7 +9,14 @@ public class Warrior : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine", 
+			"InputCore", 
+			"EnhancedInput",
+            "GameplayTags"
+        });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
