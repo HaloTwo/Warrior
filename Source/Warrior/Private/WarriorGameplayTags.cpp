4b8203cd9625c025eb9f62a870d1ba5aa56faf0e
HaloTwo <lsm6265@naver.com>
2025-05-05 14:38:50 +0900

    29

--- Source/Warrior/Private/WarriorGameplayTags.cpp
+++ Source/Warrior/Private/WarriorGameplayTags.cpp
@@ -3,9 +3,12 @@
 
 #include "WarriorGameplayTags.h"
 
-namespace WarriorGameplayTags 
+namespace WarriorGameplayTags
 {
 	//Input Tags
-	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move,"InputTag.Move");
-	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look,"InputTag.Look");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
+
+	//Player Tags
+	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe, "Player.Weapon.Axe");
 }
