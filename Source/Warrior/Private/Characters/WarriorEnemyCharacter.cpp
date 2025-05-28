d70ed795c2b3c91a63dd3530e65af3baf0e6e124
HaloTwo <lsm6265@naver.com>
2025-05-28 13:48:41 +0900

    55 비동기 로딩 및 적 스타트 데이터 준비

--- Source/Warrior/Private/Characters/WarriorEnemyCharacter.cpp
+++ Source/Warrior/Private/Characters/WarriorEnemyCharacter.cpp
@@ -4,6 +4,9 @@
 #include "Characters/WarriorEnemyCharacter.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Components/Combat/EnemyCombatComponent.h"
+#include "Engine/AssetManager.h"
+#include "DataAssets/StartUpData/DataAsset_EnemyStartUpData.h"
+#include "WarriorDebugHelper.h"
 
 AWarriorEnemyCharacter::AWarriorEnemyCharacter()
 {
@@ -23,4 +26,33 @@ AWarriorEnemyCharacter::AWarriorEnemyCharacter()
 	GetCharacterMovement()->BrakingDecelerationWalking = 1000.f;    // 걸을 때 제동력 1000 (빠르게 멈춤)
 
 	EnemyCombatComponent = CreateDefaultSubobject<UEnemyCombatComponent>(TEXT("EnemyCombatComponent"));
+}
+
+
+void AWarriorEnemyCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	InitEnemyStartUpData();
+}
+
+void AWarriorEnemyCharacter::InitEnemyStartUpData()
+{
+	if (CharacterStartUpData.IsNull()) return;
+
+
+	UAssetManager::GetStreamableManager().RequestAsyncLoad(
+		CharacterStartUpData.ToSoftObjectPath(),
+		FStreamableDelegate::CreateLambda(
+			[this]()
+			{
+				if (UDataAsset_StartUpDataBase* LoadedData = CharacterStartUpData.Get())
+				{
+					LoadedData->GiveToAbilityStstemComponent(WarriorAbilitySystemComponent);
+
+					Debug::Print(TEXT("Enemy Start Up Data Loaded 맞아 ?"), FColor::Green);
+				}
+			}
+		)
+	);
 }
