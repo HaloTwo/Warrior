24b2a59e373edb1b0bb93baa2fa0805a25ff8078
HaloTwo <lsm6265@naver.com>
2025-06-30 22:45:21 +0900

    121 근접 공격 히트 알림

--- Source/Warrior/Private/Components/Combat/EnemyCombatComponent.cpp
+++ Source/Warrior/Private/Components/Combat/EnemyCombatComponent.cpp
@@ -2,12 +2,46 @@
 
 
 #include "Components/Combat/EnemyCombatComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "WarriorGameplayTags.h"
+
 #include "WarriorDebugHelper.h"
 
 void UEnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
 {
-	if (HitActor)
+	//버랩된 액터가 이미 존재한다면 리턴
+	if (OverlappedActors.Contains(HitActor)) return;
+
+	//버랩된 액터에 추가
+	OverlappedActors.AddUnique(HitActor);
+
+	//디버그용으로 히트된 액터를 출력
+	bool bIsValidBlock = false;
+
+	const bool bIsPlayerBlocking = false;
+	const bool bIsMyAttackUnblockable = false;
+
+	//플레이어가 블록 중이고 공격이 막을 수 없는 공격이 아니라면
+	if (bIsPlayerBlocking && !bIsMyAttackUnblockable)
 	{
-		Debug::Print(GetOwningPawn()->GetActorNameOrLabel() + TEXT(" is hitting ") + HitActor->GetActorNameOrLabel());
+		//TODO::check if the block is valid
+	}
+
+	FGameplayEventData EventData;
+	EventData.Instigator = GetOwningPawn();
+	EventData.Target = HitActor;
+
+	//블록이 유효하다면
+	if (bIsValidBlock)
+	{
+		//TODO::Handle successful block
+	}
+	else
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			GetOwningPawn(),
+			WarriorGameplayTags::Shared_Event_MeleeHit,
+			EventData
+		);
 	}
 }
